f275650ea8d89f7da7419bbd9d4519606e092805
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-03-22 15:08:17 +0100

    Added endpoints for ram, disk, cpu usage

--- Makefile
+++ Makefile
@@ -1,5 +1,8 @@
 all:
-	export FLASK_APP=rpci.py && flask run
+	export FLASK_APP=rpci.py && flask run --host=0.0.0.0
+
+run:
+	python rpci.py
 
 clean:
 	find . -name \*.pyc -delete
--- rpci.py
+++ rpci.py
@@ -1,10 +1,57 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-15 -*-
 """
     RPCi
 """
 from flask import Flask
+import json
+from RPCi.lib.tools import bytes2human
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'RPCi'
+
+@app.route('/system/temp')
+def getTemperature():
+    import os
+    result = os.popen('cat /sys/class/thermal/thermal_zone0/temp').readline().replace("\n", "")
+    temperature = int(result) / 1000
+    remainder = int(result) / 100 % temperature
+
+    stat = {}
+    stat["temperature"] = str(temperature) + "." + str(remainder) + "°C"
+
+    return json.dumps(stat)
+
+@app.route('/system/cpu')
+def getCPUUsage():
+    import psutil
+    stat = {}
+    stat["usage"] = str(psutil.cpu_percent())+"%"
+    return json.dumps(stat)
+
+@app.route('/system/ram')
+def getRAMInfo():
+    import psutil
+    ram = psutil.phymem_usage()
+    stat = {}
+    stat["total"] = bytes2human(ram.total)
+    stat["used"] = bytes2human(ram.used)
+    stat["available"] = bytes2human(ram.available)
+    stat["percent"] = str(ram.percent)+"%"
+
+    return json.dumps(stat)
+
+@app.route('/system/disk')
+def getDiskInfo():
+    import psutil
+    disk = psutil.disk_usage('/')
+    stat = {}
+    stat["total"] = bytes2human(disk.total)
+    stat["used"] = bytes2human(disk.used)
+    stat["free"] = bytes2human(disk.free)
+    stat["percentage_used"] = str(disk.percent)+"%"
+
+    return json.dumps(stat)
