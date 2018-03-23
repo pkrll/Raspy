6e73f90ea2e7faeb55b5013f3cd4fc31c1318c0d
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-03-24 00:12:20 +0100

    Added toggle function for hidden files

--- client/src/components/Browse.vue
+++ client/src/components/Browse.vue
@@ -1,12 +1,15 @@
 <template>
 	<div>
+		<div class="toolbar" v-on:click="toggleHidden">
+			<font-awesome-icon v-bind:icon="toolbar.icon"/> {{toolbar.text}}
+		</div>
 		<nav class="file-list">
-			<div v-for="directory in directories" class="directory" v-if="!startsWith(directory, '.')">
+			<div v-for="directory in directories" class="directory" v-if="showHidden || !startsWith(directory.name, '.')">
 				<router-link v-bind:to="'/filesystem/browse' + directory.path">
 					<font-awesome-icon icon="folder-open"/> {{directory.name}}
 				</router-link>
 			</div>
-			<div v-for="file in files" class="file" v-if="!startsWith(file, '.')">
+			<div v-for="file in files" class="file" v-if="showHidden || !startsWith(file.name, '.')">
 				<router-link v-bind:to="'/filesystem/file' + file.path">
 					<font-awesome-icon icon="file"/> {{file.name}}
 				</router-link>
@@ -18,7 +21,7 @@
 <script>
 import {HTTP} from '../http-common'
 import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
-import { folderopen, file } from '@fortawesome/fontawesome-free-solid'
+import { folderopen, file, eye, eyeslash } from '@fortawesome/fontawesome-free-solid'
 
 export default {
 	name: 'Browse',
@@ -31,13 +34,24 @@ export default {
 	    if (!value) return false;
 	    value = value.toString();
 	    return value.charAt(0) === searchString.toString();
-	  }
+	  },
+
+		toggleHidden: function () {
+			if (this.showHidden) {
+				this.toolbar = {icon: 'eye-slash', text: 'Show hidden files'},
+				this.showHidden = false;
+			} else {
+				this.toolbar = {icon: 'eye', text: 'Hide hidden files'},
+				this.showHidden = true;
+			}
+		}
 
 	},
 
 	data() {
 		return {
 			showHidden: false,
+			toolbar: {icon: 'eye-slash', text: 'Show hidden files'},
 			files: [],
 			directories: []
 		}
@@ -59,6 +73,14 @@ export default {
 
 <style scoped>
 
+.toolbar {
+	background: rgb(92, 107, 158);
+	border-top:	1px solid #000;
+	color: 			#fff;
+	cursor: 		pointer;
+	padding:		10px;
+}
+
 .file-list div {
 	background: rgb(115, 186, 208);
 	border-top:	1px solid #000;
@@ -67,7 +89,7 @@ export default {
 .file-list a {
 	display: 					block;
 	color: 						#fff;
-	font-size: 				1.1em;
+	font-size: 				1.2em;
 	text-decoration: 	none;
 	padding: 					10px;
 	width: 						100%;
