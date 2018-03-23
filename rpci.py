b00682aec7bedb351a47a045624a76fd3f944184
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-03-23 09:36:41 +0100

    Fixing issue of missing access-control-allow-origin header

--- rpci.py
+++ rpci.py
@@ -10,8 +10,16 @@ from RPCi.core.auth import requires_auth
 
 app = Flask(__name__)
 
+# Fixes auth problems
+@app.after_request
+def after_request(response):
+  response.headers.add('Access-Control-Allow-Origin', '*')
+  response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+
+  return response
+
 @app.route('/')
-@requires_auth
 def index():
     response = {
         "resources": {
