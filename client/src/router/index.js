c7b0175dbc7b04b5bff2722fb17a666ae8dc953e
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-03-23 16:29:02 +0100

    Removed references to HelloWorld component

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,17 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 import Main from '@/components/Main'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
-      path: '/HelloWorld',
-      name: 'HelloWorld',
-      component: HelloWorld
-    },
 		{
 			path: '/',
 			name: 'Main',
