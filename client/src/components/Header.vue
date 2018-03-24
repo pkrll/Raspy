7626acd1d32a4bb11c7b5e7127a0427d46ebb3c7
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-03-24 02:03:50 +0100

    Changed title and made header a link

--- client/src/components/Header.vue
+++ client/src/components/Header.vue
@@ -1,8 +1,8 @@
 <template>
-	<header>
+	<a href="/" class="header">
 		<img src="../assets/img/pi.svg">
-		<div class="title">Raspberry Pi Command Interface</div>
-	</header>
+		<div class="title">Raspberry Pi Control Interface</div>
+	</a>
 </template>
 
 <script>
@@ -13,22 +13,27 @@ export default {
 
 <style scoped>
 
-header {
+.header {
 	background: 			rgb(23,173,145);
 	display: 					flex;
 	align-items:			center;
 	justify-content:	flex-start;
 	padding:					10px;
+	text-decoration:	none;
 }
 
-header > div.title {
+.header > div.title {
 	color: 				rgb(254,254,252);
 	font-size: 		1.3em;
 	margin-left:	20px;
 }
 
-header > img {
+.header img {
 	height: 64px;
+}
+
+.header:hover div.title {
+	color: #000;
 }
 
 </style>
