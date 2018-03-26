2101bb0e526442b2cd17ce4cc9766c488ef4cda0
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-03-26 21:48:40 +0200

    Added the actual spinner component

---
+++ client/src/components/Filesystem/DirectoryListing.vue
@@ -0,0 +1,83 @@
+<template>
+	<nav class="file-list-view" v-if="files.length > 0 || directories.length > 0">
+		<div class="item" v-if="this.prettyPath != '/'">
+			<router-link :to="{ name: 'Browse path', params: {path: encodeURIComponent(this.parentPath) }}">
+				<span><font-awesome-icon icon="folder-open"/>&nbsp;&nbsp;...</span>
+			</router-link>
+		</div>
+
+		<div class="item" v-for="directory in directories" v-if="showHidden || !startsWith(directory.name, '.')">
+			<router-link :to="{ name: 'Browse path', params: {path: encodeURIComponent(directory.path) }}">
+				<span><font-awesome-icon icon="folder-open"/>&nbsp;&nbsp;{{directory.name}}</span>
+			</router-link>
+		</div>
+
+		<div class="item" v-for="file in files" v-if="showHidden || !startsWith(file.name, '.')">
+			<router-link :to="{ name: 'File', params: {path: encodeURIComponent(file.path) }}">
+				<span><font-awesome-icon icon="file"/>&nbsp;&nbsp;{{file.name}}</span>
+			</router-link>
+		</div>
+	</nav>
+	<nav class="file-list-view" v-else>
+		<div class="item" v-if="this.prettyPath != '/'">
+			<router-link :to="{ name: 'Browse path', params: {path: encodeURIComponent(this.parentPath) }}">
+				<span><font-awesome-icon icon="folder-open"/>&nbsp;&nbsp;...</span>
+			</router-link>
+		</div>
+	</nav>
+</template>
+
+<script>
+import shared from '@/common'
+import FontAwesomeIcon from '@fortawesome/vue-fontawesome'
+import { folderopen, file } from '@fortawesome/fontawesome-free-solid'
+
+export default {
+	props: ['directories', 'files', 'showHidden', 'prettyPath'],
+	name: "DirectoryListing",
+	components: { FontAwesomeIcon },
+	computed: {
+		/**
+		 * Returns the parent path of the current path, or itself if the path is the root folder.
+		 *
+		 * @return {String} The parent path of the current path.
+		 */
+		parentPath: function () {
+			let _path = this.prettyPath;
+			let index = _path.lastIndexOf('/');
+
+			if (index > 0)  return _path.substring(0, index);
+			if (index == 0) return '/';
+
+			return _path;
+		}
+	},
+	created() {
+		this.startsWith = shared.startsWith;
+	}
+}
+</script>
+
+<style>
+
+.file-list-view {
+
+}
+
+.item a {
+	display: 					block;
+	color: 						rgb(255,255,255);
+	padding: 					0px 0 0px 20px;
+	text-decoration: 	none;
+	width: 						100%;
+}
+
+.item {
+	background: rgb(45, 49, 57);
+}
+
+.item:nth-child(even) {
+	background: rgb(32,35,44);
+}
+
+</style>
