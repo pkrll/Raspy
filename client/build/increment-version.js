4f7b634813bac3c239cb3cd5d482c94118bf5037
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-03-29 13:54:03 +0200

    Updated increment version script to include --reset option

--- client/build/increment-version.js
+++ client/build/increment-version.js
@@ -3,6 +3,9 @@ var fs = require('fs');
 
 let semindex 	= 2;
 let incrBuild = true;
+let rsetBuild = false;
+let rsetPatch = false;
+let rsetMinor = false;
 
 let argv = process.argv.slice(2);
 
@@ -18,6 +21,12 @@ if (argv.length > 0) {
 			}
 		} else if (value == '--skip-build') {
 			incrBuild = false;
+		} else if (value == '--reset-build') {
+			rsetBuild = true;
+		} else if (value == '--reset-patch') {
+			rsetPatch = true;
+		} else if (value == '--reset-minor') {
+			rsetMinor = true;
 		}
 	}
 }
@@ -29,9 +38,16 @@ fs.readFile('package.json', function(err, data) {
 	let version = content.version.split('+');
 
 	if (version.length > 0) {
+		let semver = version[0].split('.');
+
 		if (semindex >= 0) {
+			if (semindex != 2 && rsetPatch) {
+				semver[2] = 0;
+			} else if (semindex != 1 && rsetMinor) {
+				semver[1] = 0;
+			}
+
 			console.log("Incrementing version number...");
-			let semver = version[0].split('.');
 			semver[semindex] = parseInt(semver[semindex]) + 1;
 			version[0] = semver.join('.');
 		}
@@ -39,6 +55,8 @@ fs.readFile('package.json', function(err, data) {
 		if (incrBuild) {
 			console.log("Incrementing build number...");
 			version[1] = (parseInt("0x"+version[1]) + 1).toString(16);
+		} else if (rsetBuild) {
+			version[1] = 0;
 		}
 	}
 
