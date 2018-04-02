589ea6912151b128079536e5b4ecc885981831dc
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-04-02 20:01:00 +0200

    CORS requests are now enabled only when running in dev mode

--- server/server.js
+++ server/server.js
@@ -1,19 +1,17 @@
 #!/usr/bin/env node
 const express     = require('express');
 const app         = express();
-const cors        = require('cors');
 const bodyParser  = require('body-parser');
 const path        = require('path');
 
 // CONFIGURATIONS
 require('./src/config')(app, 5000, path.dirname(__dirname));
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-
 // ROUTES
 require('./src/router')(app);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.listen(app.get('port'));
 console.log('Server started on ' + app.get('port'));
--- server/src/config/index.js
+++ server/src/config/index.js
@@ -1,8 +1,14 @@
 'use strict';
 const express = require('express');
 const path    = require('path');
+const cors    = require('cors');
 
 module.exports = function(app, defaultPort, rootFolder) {
   app.set('port', process.env.PORT || defaultPort);
   app.set('dist', path.join(rootFolder, 'dist'));
+
+  if (process.env.NODE_ENV != 'production') {
+    console.log("Enabling all CORS requests");
+    app.use(cors());
+  }
 };
