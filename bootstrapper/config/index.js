524cca73ef08bff1e27af6d13ebd2f561234b45b
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-23 18:09:22 +0200

    Fixed issue with bootstrapper crashing if there is no log folder

--- bootstrapper/config/index.js
+++ bootstrapper/config/index.js
@@ -1,12 +1,13 @@
 'use strict'
 
 module.exports = {
+  logPath: 'logs',
   dev: {
     port: 5001,
-    databasePath: 'db.json'
+    databasePath: 'db.json',
   },
   prod: {
     port: 5001,
-    databasePath: 'db.json'
+    databasePath: 'db.json',
   }
 }
--- bootstrapper/helpers/config.js
+++ bootstrapper/helpers/config.js
@@ -1,17 +1,21 @@
 'use strict';
 const express = require('express');
 const path    = require('path');
+const fs      = require('fs');
 const config	= require('../config/')
 const winston = require('winston');
 
 module.exports = function(app) {
   const isDebugMode  = (process.env.NODE_ENV != 'production');
-  const defaultPort  = (isDebugMode) ? config.dev.port : config.port.port;
-  const databasePath = (isDebugMode) ? config.dev.databasePath : config.port.databasePath;
+  const defaultPort  = (isDebugMode) ? config.dev.port : config.prod.port;
+  const databasePath = (isDebugMode) ? config.dev.databasePath : config.prod.databasePath;
 
   app.set('databasePath', databasePath);
   app.set('port', process.env.PORT || defaultPort);
   app.set('dist', path.join(__dirname, '../dist'));
+
+  let logPath = path.join(__dirname, '/../'+config.logPath);
+  if (fs.existsSync(logPath) == false) { fs.mkdirSync(logPath); }
 
   winston.loggers.add('command-logger', {
      console: {
@@ -19,7 +23,7 @@ module.exports = function(app) {
        colorize: true,
        label: 'Command history'
      },
-     file: { filename: 'tmp/commands.log' }
+     file: { filename: logPath + '/commands.log' }
   });
 
   winston.loggers.add('logger', {
@@ -28,6 +32,6 @@ module.exports = function(app) {
        colorize: true,
        label: 'Logger'
      },
-     file: { filename: 'tmp/info.log' }
+     file: { filename: logPath + '/info.log' }
   });
 };
