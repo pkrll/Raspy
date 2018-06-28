eacb395745ee10f1ef727b7e1b649536de3a1bad
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-06-29 00:59:31 +0200

    Added empty vue components and js files for different settings pages

---
+++ rasbot/web/src/components/Settings/Account/Account.vue
@@ -0,0 +1,12 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {
+  name: 'SettingsAccount'
+}
+</script>
+
+<style scoped src="@/components/Settings/main.css">
+</style>
---
+++ rasbot/web/src/components/Settings/Dashboard/Dashboard.vue
@@ -0,0 +1,12 @@
+<template>
+  <section></section>
+</template>
+
+<script>
+export default {
+  name: 'SettingsDashboard'
+}
+</script>
+
+<style scoped src="@/components/Settings/main.css">
+</style>
