96a8795a09cae4270fa143c8e41d228f3c1dddb7
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-07-01 15:41:15 +0200

    Improved password change logic client side

--- raspbot/web/src/components/Settings/Account/Password/password.js
+++ raspbot/web/src/components/Settings/Account/Password/password.js
@@ -3,19 +3,29 @@
 exports.data = function() {
   return {
     password: '',
-    message: ''
+    message: '',
+    isDisabled: false
   }
 };
 
 exports.methods = {
   updatePassword: function() {
-    if (this.password.length > 0) {
+    if (this.password.length < 3) {
+      this.message = 'Password must be more than 3 characters long.'
+      return;
+    }
+
+    if (this.isDisabled == false) {
+      this.message = "Updating password..."
+      this.isDisabled = true;
       this.$APIManager.updatePassword(this.password, response => {
         if (response.success) {
           this.message = 'Password changed!';
         } else {
           this.message = 'Could not change password'
         }
+
+        this.isDisabled = false;
       });
     }
   }
