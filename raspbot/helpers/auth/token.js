9146b861188e3dcbd78165a4886309346cdadca1
Ardalan Samimi <ardalan.samimi@gmail.com>
2018-07-04 17:45:38 +0200

    Added job scheduler to purge old tokens

--- raspbot/helpers/auth/token.js
+++ raspbot/helpers/auth/token.js
@@ -1,8 +1,18 @@
 'use strict'
 const Memory = require('lowdb/adapters/Memory');
 const lowdb  = require('lowdb')(new Memory());
+const cronjob = require('cron').CronJob;
 
 lowdb.defaults({ tokens: [] }).write();
+// Clean up tokens older than 24 hours every day
+const job = new cronjob('* 00 00 * * *', function() {
+	console.log("Cleaning up old tokens...");
+	const dateLimit = 24 * 60 * 60 * 1000; // 24 hours
+	lowdb.get('tokens').remove(data => {
+		const date = ((new Date()) - data.loggedIn);
+		return (date > dateLimit);
+	}).write();
+}, null, true);
 
 exports.generate = callback => {
 	require('crypto').randomBytes(24, (error, buffer) => {
@@ -15,7 +25,12 @@ exports.generate = callback => {
 };
 
 exports.save = (token, clientIP, username) => {
-	lowdb.get('tokens').push({ token: token, clientIP: clientIP, username: username }).write();
+	lowdb.get('tokens').push({
+		token: token,
+		clientIP: clientIP,
+		username: username,
+		loggedIn: new Date()
+	}).write();
 };
 
 exports.check = (token, clientIP) => {
